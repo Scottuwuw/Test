b7aa0154917a27eb1ec26b1f241884830478bb6c
Scottuwuw <100088560+Scottuwuw@users.noreply.github.com>
2023-03-29 22:36:35 -0600

    Add files via upload

---
+++ resultados.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultado del formulario</title>
+</head>
+<body>
+	<h1>Resultado del formulario</h1>
+	<p>Nombre: <?php echo $_POST["nombre"]; ?></p>
+	<p>Email: <?php echo $_POST["email"]; ?></p>
+</body>
+</html>
+
